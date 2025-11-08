aec2b40cbe5756afd12d2f77b1b8403937c3ca99
Danil Chugaev <dmchugaev@gmail.com>
2025-11-09 03:05:42 +0700

    upd

--- docs/.vuepress/.temp/pages/go/pointers/index.html.vue
+++
@@ -1,4 +0,0 @@
-<template><div><h1 id="указатели" tabindex="-1"><a class="header-anchor" href="#указатели"><span>Указатели</span></a></h1>
-</div></template>
-
-
--- docs/.vuepress/.temp/pages/guide/pointers/index.html.vue
+++
@@ -1,4 +0,0 @@
-<template><div><h1 id="глава-13-указатели" tabindex="-1"><a class="header-anchor" href="#глава-13-указатели"><span>Глава 13 - Указатели</span></a></h1>
-</div></template>
-
-
